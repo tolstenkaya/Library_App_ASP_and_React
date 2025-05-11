826db445ca5eea5806385658cf60edd214d0d9d2
tolstenkaya <o.tutova.82@gmail.com>
2025-05-11 17:40:40 +0300

    Add service LibraryService, add controller LibtatyController, remove WeatherForecast controller and model

---
+++ Controllers/LibraryController.cs
@@ -0,0 +1,24 @@
+﻿using Library_App_ASP_and_React.Models;
+using Library_App_ASP_and_React.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Library_App_ASP_and_React.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class LibraryController : ControllerBase
+    {
+        private readonly LibraryService libraryService;
+        public LibraryController(LibraryService _libraryService)
+        {
+            libraryService = _libraryService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> getBooks()
+        {
+            return libraryService.GetAllBooks();
+        }
+    }
+}
