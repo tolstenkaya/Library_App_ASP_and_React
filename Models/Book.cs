aa6ad604cca7cb92dd14bce1bb7e5277f1f451a6
tolstenkaya <o.tutova.82@gmail.com>
2025-05-11 17:16:18 +0300

    Create model class Book

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Library_App_ASP_and_React.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publishing_House { get; set; }
+        public DateTime Publishing_Date { get; set; }
+        public string Image {  get; set; }
+    }
+}
