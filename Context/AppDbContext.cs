85d70586d427793b070e114a1fb8878453baacb9
tolstenkaya <o.tutova.82@gmail.com>
2025-05-24 14:21:32 +0300

    Improve security of connection to db

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -6,14 +6,13 @@ namespace Library_App_ASP_and_React.Context
     public class AppDbContext : DbContext
     {
         public DbSet<Book> Books { get; set; } = null!;
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-           : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-2V9CUIT;Database=Library;Trusted_Connection=True;TrustServerCertificate=True");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
     }
 }
